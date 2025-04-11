c72dc350c44a417a667ac52155f0f08fba9fc04b
Pete MC-Mods <mcmods.pete@gmail.com>
2025-04-11 18:43:58 +0200

    small changes for 1.21.5, fix for crash on 1.21.5

--- src/main/java/net/petemc/daycount/client/DayCountHud.java
+++ src/main/java/net/petemc/daycount/client/DayCountHud.java
@@ -1,11 +1,9 @@
 package net.petemc.daycount.client;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.LayeredDraw;
-import net.minecraft.client.renderer.GameRenderer;
 import net.petemc.daycount.DayCount;
 import net.petemc.daycount.config.MainConfig;
 import org.jetbrains.annotations.NotNull;
@@ -24,15 +22,13 @@ public class DayCountHud implements LayeredDraw.Layer {
             assert mc.level != null;
             int currentDay = (int) (mc.level.getDayTime() / 24000L);
             assert mc.gameMode != null;
-            if (mc.gameMode.hasExperience() || mc.gameMode.hasInfiniteItems()) {
+            if (mc.gameMode.getPlayerMode().isSurvival() || mc.gameMode.getPlayerMode().isCreative()) {
                 PoseStack matrixStack = guiGraphics.pose();
                 matrixStack.pushPose();
                 matrixStack.translate(MainConfig.getLocationX(), MainConfig.getLocationY(), 0);
                 matrixStack.scale(MainConfig.getSizeX(), MainConfig.getSizeY(), 2.5f);
 
-                RenderSystem.setShader(GameRenderer::getPositionTexShader);
-                RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-                guiGraphics.drawString(mc.font, "Day: " + (currentDay + MainConfig.getDayOffset()), 2, 2, MainConfig.getTextColor());
+                guiGraphics.drawString(mc.font, "Day: " + (currentDay + MainConfig.getDayOffset()), 1, 1, MainConfig.getTextColor());
                 matrixStack.popPose();
             }
         }
