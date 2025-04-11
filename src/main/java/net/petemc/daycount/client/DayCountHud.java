273c554612f17b33a1582be5fad9eb5ca33751d5
Pete MC-Mods <mcmods.pete@gmail.com>
2025-04-11 18:45:17 +0200

    fix crash on 1.21.5

--- src/main/java/net/petemc/daycount/client/DayCountHud.java
+++ src/main/java/net/petemc/daycount/client/DayCountHud.java
@@ -23,7 +23,7 @@ public class DayCountHud implements LayeredDraw.Layer {
             assert mc.level != null;
             int currentDay = (int) (mc.level.getDayTime() / 24000L);
             assert mc.gameMode != null;
-            if (mc.gameMode.hasExperience() || mc.gameMode.hasInfiniteItems()) {
+            if (mc.gameMode.getPlayerMode().isSurvival() || mc.gameMode.getPlayerMode().isCreative()) {
                 PoseStack matrixStack = guiGraphics.pose();
                 matrixStack.pushPose();
                 matrixStack.translate(MainConfig.getLocationX(), MainConfig.getLocationY(), 0);
